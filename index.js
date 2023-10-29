504e8adf522aa4649d93985877f96264f9b3bb9f
Diamond Francis <hello@diamondfrancis.co.uk>
2023-10-29 23:41:08 +0000

    changes

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Connect to MongoDB
+const uri = "mongodb+srv://diamondfrancisdesign:grp0w6OKPqPdf54N@cluster0.vf3ygbf.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect('mongodb://localhost:27017/bblog', {
     useNewUrlParser: true,
     useUnifiedTopology: true
+});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 //schema
@@ -15,17 +25,20 @@ const postSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now }
 });
 
-const Post = mongoose.model('Post', postSchema);
-
 // Set EJS as view engine
 app.set('view engine', 'ejs');
 
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("Connected to MongoDB!");
-});
 
 // Start the server
 app.listen(3000, () => {
